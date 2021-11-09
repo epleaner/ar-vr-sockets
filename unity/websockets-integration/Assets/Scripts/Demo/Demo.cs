4f222fe16e7aba2c668184b94b1dd941d30d9d15
Eli Pleaner <eliipleaner@gmail.com>
2021-11-09 13:59:34 -0800

    Add unity

---
+++ unity/websockets-integration/Assets/Scripts/Demo/Demo.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using KyleDulce.SocketIo;
+
+public class Demo : MonoBehaviour
+{
+    Socket s;
+    // Start is called before the first frame update
+    void Alive()
+    {
+        s = SocketIo.establishSocketConnection("ws://localhost:4001");
+        s.connect();
+        Debug.Log("Connected in Alive");
+    }
+
+    void OnDisable()
+    {
+        s.close();
+    }
+
+    void Update()
+    {
+        if(s != null) { 
+            s.emit("getMarkers", "all");
+            s.on("Markers", onMarkerPosition); 
+        }
+        else {
+            s = SocketIo.establishSocketConnection("ws://localhost:4001");
+            s.connect();
+            Debug.Log("Connected in Update");
+        }
+    }
+
+    void call(string d) {
+        Debug.Log("RECIEVED EVENT: " + d);
+    }
+
+    void onMarkerPosition(string d) {
+        Debug.Log("RECIEVED EVENT: Marker Position" + d);
+    }
+
+    void onMarkerLost(string d) {
+        Debug.Log("RECIEVED EVENT: Marker Lost" + d);
+    }
+}
